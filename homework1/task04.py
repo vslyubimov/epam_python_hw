8a63e158c5636f9fc4fd3a4f83f2b869f8c4b9e0
vslyubimov <vslyubimov@yandex.ru>
2021-09-08 22:16:34 +0300

    add CI

--- homework1/task04.py
+++ homework1/task04.py
@@ -1,8 +1,9 @@
 from typing import List
 
+
 def check_sum_of_four(a: List[int], b: List[int], c: List[int], d: List[int]) -> int:
     counter = 0
-    for i in range (0, len(a)):
+    for i in range(0, len(a)):
         if a[i] + b[i] + c[i] + d[i] == 0:
             counter += 1
         else:
--- tests/homework1/test_task02.py
+++ tests/homework1/test_task02.py
@@ -1,19 +1,18 @@
 from homework1.task02 import check_fibonacci
 
-
 check1 = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 check2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 check3 = [25, 26, 3]
 
 
 def test_task_02_Positive():
-
     assert check_fibonacci(check1)
 
 
 def test_task_02_Negative():
-
     assert not check_fibonacci(check2)
+
+
 """
 доделаю код с 2 и менее элементами для этой проверки
 def test_task_02_Negative_two_elements():
